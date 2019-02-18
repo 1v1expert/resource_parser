785bffc475e524eae5c20d7d1edf8e35766677f2
1v1expert <1v1expert@gmail.com>
2019-02-19 00:56:16 +0300

    Update project and contains data

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+
+def render_page(request):
+    template = loader.get_template('base.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
