c69aeeb677a6d4d6618b19738925c9b7860bc68d
1v1expert <1v1expert@gmail.com>
2019-02-20 01:33:16 +0300

    Update template and added templatetags and filling content

--- core/views.py
+++ core/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse, JsonResponse
+from core.models import Cars
 
 
 def render_page(request):
     template = loader.get_template('base.html')
-    context = {}
+    context = {'Cars': Cars.objects.all()}
     return HttpResponse(template.render(context, request))
