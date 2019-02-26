aac266d51f05eb2597a1d24b2a8e80fe2429b245
1v1expert <1v1expert@gmail.com>
2019-02-26 21:16:08 +0300

    Update a view page

--- core/views.py
+++ core/views.py
@@ -10,16 +10,13 @@ def render_page(request):
     return HttpResponse(template.render(context, request))
 
 def render_page_points(request):
-    print('He')
+
     template = loader.get_template('points.html')
     part1 = OzonPoints.objects.all()[0:999]
     part2 = OzonPoints.objects.all()[999:1998]
-    tagnews = []
     payt_list = list()
     payt_list += list(part1)
     payt_list += list(part2)
-    tagnews.append(list(part1))
-    tagnews.append(list(part2))
-    print(tagnews)
+
     context = {'Points': payt_list}
     return HttpResponse(template.render(context, request))
